417cafbc337126ebaf02310286312f78f5a393de
BLB Avishka <buddhimaliyanageba@gmail.com>
2025-03-16 11:42:05 +0530

    Create move background animation

--- game.js
+++ game.js
@@ -3,16 +3,35 @@ let flyImageNumber = 0;
 let flyAnimationNumber = 0;
 
 function flyAnimation() {
-  flyImageNumber = flyImageNumber +1;
+  flyImageNumber = flyImageNumber + 1;
 
-  if (flyImageNumber == 9){
+  if (flyImageNumber == 9) {
     flyImageNumber = 1;
   }
 
-  document.querySelector(".bird").src = "Images/Game Page/Fly Bird/" + flyImageNumber + ".png";
-
+  document.querySelector(".bird").src =
+    "Images/Game Page/Fly Bird/" + flyImageNumber + ".png";
 }
 
 function flyAnimationStart() {
-  flyAnimationNumber = setInterval(flyAnimation,140);
+  flyAnimationNumber = setInterval(flyAnimation, 140);
+}
+
+function keyCheck(event) {
+  // alert(event.which);
+  // enter = 13;
+
+  if (event.keyCode === 13 && moveBackgroundAnimationId === 0) {
+    moveBackgroundAnimationId = setInterval(moveBackground, 100);
+  }
+}
+
+var backgroundImagePositionX = 0;
+var moveBackgroundAnimationId = 0;
+
+function moveBackground() {
+  backgroundImagePositionX = backgroundImagePositionX - 20;
+
+  document.getElementById("background").style.backgroundPositionX =
+    backgroundImagePositionX + "px";
 }
