9ed10dcfe6539f9166b4efbfc0b90e4eabf2d0f4
BLB Avishka <buddhimaliyanageba@gmail.com>
2025-03-16 19:45:23 +0530

    Added obstacles and started the score count

--- game.js
+++ game.js
@@ -26,6 +26,10 @@ function keyCheck(event) {
 
   if (event.keyCode === 13 && moveBackgroundAnimationId === 0) {
     moveBackgroundAnimationId = setInterval(moveBackground, 100);
+
+    if (obstacleAnimationId === 0) {
+      obstacleAnimationId = setInterval(obstacleAnimation, 100);
+    }
   }
 
   if (event.keyCode === 32) {
@@ -42,16 +46,21 @@ function keyCheck(event) {
 var backgroundImagePositionX = 0;
 var moveBackgroundAnimationId = 0;
 
+var score = 0;
+
 function moveBackground() {
   backgroundImagePositionX = backgroundImagePositionX - 20;
 
   document.getElementById("background").style.backgroundPositionX =
     backgroundImagePositionX + "px";
+  
+  score = score + 1;
+  document.getElementById("score").innerHTML = score;  
 }
 
 var birdPosition = 350;
 
-function up(){
+function up() {
   birdPosition = birdPosition - 10;
 
   document.getElementById("bird").style.top = birdPosition + "px";
@@ -61,4 +70,49 @@ function down() {
   birdPosition = birdPosition + 10;
 
   document.getElementById("bird").style.top = birdPosition + "px";
+}
+
+obstacleMarginLeft = 1500;
+
+function createObstacle() {
+  for (var i = 0; i <= 10; i++) {
+    var obstacle = document.createElement("div");
+    obstacle.className = "obstacle";
+    document.getElementById("background").appendChild(obstacle);
+    obstacle.style.marginLeft = obstacleMarginLeft + "px";
+    obstacle.id = "obstacle" + i;
+
+    // obstacleMarginLeft = obstacleMarginLeft + 200;
+
+    if (i < 5) {
+      obstacleMarginLeft = obstacleMarginLeft + 2000;
+    }
+
+    if (i >= 5) {
+      obstacleMarginLeft = obstacleMarginLeft + 1000;
+    }
+  }
+}
+
+var obstacleAnimationId = 0;
+
+function obstacleAnimation() {
+  for (var i = 0; i <= 10; i++) {
+    var obstacle = document.getElementById("obstacle" + i);
+    var currentMarginLeft = getComputedStyle(obstacle).marginLeft;
+    var newMarginLeft = parseInt(currentMarginLeft) - 25;
+    obstacle.style.marginLeft = newMarginLeft + "px";
+
+    if (newMarginLeft >= -110 && newMarginLeft <= 200) {
+      if (birdPosition >= 200 && birdPosition <= 320) {
+        clearInterval(obstacleAnimationId);
+
+        clearInterval(flyAnimationNumber);
+        flyAnimationNumber = -1;
+
+        clearInterval(moveBackgroundAnimationId);
+        moveBackgroundAnimationId = -1;
+      }
+    }
+  }
 }
