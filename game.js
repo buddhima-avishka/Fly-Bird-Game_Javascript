9970d37f5f9106afa902dc2e4317982e0197d94f
BLB Avishka <buddhimaliyanageba@gmail.com>
2025-03-16 13:02:37 +0530

    Changed bird's position and paused the game when press keys

--- game.js
+++ game.js
@@ -20,9 +20,22 @@ function flyAnimationStart() {
 function keyCheck(event) {
   // alert(event.which);
   // enter = 13;
+  // space = 32;
+  // w = 119;
+  // s = 115;
 
   if (event.keyCode === 13 && moveBackgroundAnimationId === 0) {
     moveBackgroundAnimationId = setInterval(moveBackground, 100);
+  }
+
+  if (event.keyCode === 32) {
+    alert("Game Paused");
+  }
+
+  if (event.keyCode === 119) {
+    up();
+  } else if (event.keyCode === 115) {
+    down();
   }
 }
 
@@ -34,4 +47,18 @@ function moveBackground() {
 
   document.getElementById("background").style.backgroundPositionX =
     backgroundImagePositionX + "px";
+}
+
+var birdPosition = 350;
+
+function up(){
+  birdPosition = birdPosition - 10;
+
+  document.getElementById("bird").style.top = birdPosition + "px";
+}
+
+function down() {
+  birdPosition = birdPosition + 10;
+
+  document.getElementById("bird").style.top = birdPosition + "px";
 }
